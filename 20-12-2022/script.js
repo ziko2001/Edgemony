6744aba14cb1a6d1c7e44f35e313418f0c0a9a63
ziko2001 <121048461+ziko2001@users.noreply.github.com>
2022-12-20 18:07:30 +0100

    esercizio del 20-12-2022

---
+++ 20-12-2022/script.js
@@ -0,0 +1,18 @@
+let stocks = [];
+
+
+for (let i = 0; i < 10; i++ ) {
+    stocks.push(prompt("Elencami 10 azioni del momento")); 
+    console.log(stocks);
+}
+
+/*
+for (let stock of stocks){ 
+    console.log(stock);
+
+}
+
+for (let stockIndex in stocks) {
+    console.log(stockIndex);
+}
+*/
