3d23ac6f6ff51915d9f3bd3c74415bfeb9457ee0
Zakaria Boujebha <121048461+ziko2001@users.noreply.github.com>
2023-01-11 18:10:16 +0100

    esercizio del 11-01-2023

---
+++ 11-01-2023/script.js
@@ -0,0 +1,19 @@
+function cardColorChange() {
+    const title = document.querySelector(".title");
+    const titolo = document.querySelector(".titolo");
+    titolo.style.color = "black";
+  
+ }
+  
+   const changeColor = document
+   .querySelector(".card")
+   .addEventListener("click", titleColorchange);
+
+   function deletCardButton() {
+    const section = document.querySelector(".card");
+
+   }
+
+   const deleteCard = document
+   .querySelector(".button")
+   .addEventListener("click", deletCardButton);
