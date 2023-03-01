290bb7d2471b3e2cfb43cda77dd76c9806ab4c9d
Zakaria Boujebha <121048461+ziko2001@users.noreply.github.com>
2023-03-01 09:50:43 +0100

    esercizio del 28-02-2023

---
+++ 28-02-2023/App.jsx
@@ -0,0 +1,21 @@
+
+import "./App.css";
+
+
+
+
+const Header = () => {
+  return (
+    <header className="Header">
+      <nav>
+        <ul>
+          <li><a href="hhttps://coinmarketcap.com/">Home</a></li>
+          <li><a href="https://coinmarketcap.com/about/">About</a></li>
+          <li><a href="https://coinmarketcap.com/faq/">FAQ</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
